ed4bad832d0a5668264b9c6ef37f11ae16429c83
Tom Dwyer <tom.dwyer@extropy.io>
2020-07-16 18:56:02 +0100

    First working fileset

---
+++ pay_satoshi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+COMMAND="lncli sendpayment --keysend --dest=031920d584984042b198a2a8e5d0608a7523bac56948939580a15b1fd3e4c98e2a --amt=1 --final_cltv_delta=40"
+for i in {1..20}
+do
+        eval "$COMMAND" 
+        echo "Sent $i satoshis"
+done
